6c052af8e859c9e4596292a1781bbf3e4ef0dfdf
manojnitte58-cyber <manojnitte58@gmail.com>
2026-02-25 09:57:07 +0530

    Implement sum calculation in sum.java
    Fix syntax errors and improve output statements.

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class sum
+  {
+    public Static void main(String args[])
+    {
+      Scanner S=new Scanner(System.in);
+      System.out.println("enter a value");
+      int a=S.nextInt();
+      system.out.println("enter b value"); 
+      int b=S.nextInt();
+      int sum=a+b;
+    system.out.print("sum of two number");
+  }
+}
